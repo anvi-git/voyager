82341122a0f4d4133fc449bb59b27e8b5898d30e
anvi-git <antonio.viscusi@edu.unito.it>
2025-03-31 19:17:17 +0200

    new

---
+++ backyard-thoughts-react/src/components/Banner.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <section id="litposts">
+        <h2><a href="/literary-garden">a literary garden</a></h2>
+      </section>
+      <section id="lss">
+        <h2><a href="/cosmological-garden">a cosmological garden</a></h2>
+      </section>
+      <section id="spso">
+        <h2><a href="/musical-garden">a musical garden</a></h2>
+      </section>
+      <section id="about_me">
+        <h2><a href="/about-me">about me</a></h2>
+      </section>
+    </div>
+  );
+};
+
+export default Banner;

4a140e6cfc97cfcaf58f5fae12ac809018d98f7b
anvi-git <antonio.viscusi@edu.unito.it>
2025-09-01 21:15:58 +0200

    new

--- blog_data.js
+++ blog_data.js
@@ -1,97 +1,5 @@
 window.BLOG_ARTICLES = {
   '1': {
-    title: 'a musical suggestion: Maribou State',
-    date: '2025-02-08',
-    content: `
-      <p>This is what I'm listening to lately:</p>
-      <iframe allow='autoplay *; encrypted-media *;' frameborder='0' height='450' style='width:60%;max-width:660px;overflow:hidden;background:transparent;border-radius:12px;margin-bottom:20px;' sandbox='allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation' src='https://embed.music.apple.com/it/album/hallucinating-love/1716587925?l=en-GB'></iframe> <br>
-      <iframe style="border-radius:12px;width:60%;height:450px;" src="https://open.spotify.com/embed/album/6NyoU5F25y1I7V9fqbKqy9?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-      <p>Maribous State is a duo from Hertfordshire, England. <br>
-        They compose electronic music. <br>
-        You can find Maribou State on 
-        <a href='https://mariboustate.bandcamp.com/album/hallucinating-love' target='_blank'>Bandcamp</a>, 
-        <a href='https://music.apple.com/us/album/hallucinating-love/1716587925' target='_blank'>Apple Music</a>, 
-        <a href='https://open.spotify.com/album/6NyoU5F25y1I7V9fqbKqy9' target='_blank'>Spotify</a>, 
-        <a href='https://youtu.be/UuaQUpu8Sh0?list=OLAK5uy_nGGVqg_13qm0yacu_qP0T7jAiR9waAY80' target='_blank'>YouTube</a>.
-        <br>Hallucinating Love was published on Jan 31, 2025 
-      </p>
-      <hr>
-    `
-  },
-  '2': {
-    title: 'on Sulfur, Dante, and Gustave Doré',
-    date: '2025-02-09',
-    content: `
-        <p>Today I was in a conversation about sulfur and its connection with the devilish figures of literature. <br>
-          Then I thought about the smell of sulfur and how it is used in the Bible to describe the smell of hell. <br>
-          Then I thought about the idea of hell in literature. <br>
-          And I figured the beautiful art of <a href='https://www.wikiart.org/en/gustave-dore' target='_blank'>Gustave Doré</a>. <br>
-          And I remembered the illustrations he made for Dante's Divine Comedy. <br> 
-          This is one of many: <br>
-          <a href='https://cdn8.openculture.com/2019/02/16110925/cantox.jpg' target='_blank'>Inferno, Canto X</a>.
-        </p>
-        <div class="image-container">
-          <img src="images/posts/cantox_dore.jpg" alt="Inferno, Canto X. Gustave Doré" style="width:100%;max-width:660px;">
-          <div class="caption">Inferno, Canto X. Gustave Doré</div>
-        </div>
-        <hr>
-    `
-  },
-  '3': {
-    title: 'Bon Iver announcement',
-    date: '2025-02-10',
-    content: `
-        <p>Today <a href='https://boniver.org/' target='_blank'> Bon Iver </a> announced a new album. <br>
-            Which is great news. <br>
-            The new album, called 
-            <a href='https://bon-iver.lnk.to/SABLE-fABLE?omnisendContactID=6532f01c26c972a2b9cdb503&utm_campaign=campaign%3A+fABLE+Announce+%2867a68601c62b8563590de4a3%29&utm_medium=email&utm_source=omnisend#music-services' target='_blank'> 'SABLE, fABLE'</a> 
-            will be released on April 11, 2025. <br>
-          </p>
-          <div class="image-container">
-            <img src="images/posts/SABLE_fABLE.jpg" alt="the new album cover.  all rights reserved. " style="width:100%;max-width:660px;">
-            <div class="caption">
-              the new album cover.  all rights reserved.
-            </div>
-          </div>
-          <hr>
-    `
-  },
-  '4': {
-    title: 'a brief reminder',
-    date: '2025-02-12',
-    content: `
-              <p> 
-              Today, a brief reminder that not every change must be visible.<br>
-              Some things take time, and change is barely perceivable. <br>
-              </p>
-              <iframe allow='autoplay *; encrypted-media *;' frameborder='0' height='150' style='width:60%;max-width:660px;overflow:hidden;background:transparent;border-radius:12px;margin-bottom:20px;' sandbox='allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation' src="https://embed.music.apple.com/it/album/intro-spectrum/1589172754?i=1589172966"></iframe> <br>
-              <iframe style="border-radius:12px;width:60%;height:162px;" src="https://open.spotify.com/embed/track/3PQH6w0yfM2ewcIGmKd2yL?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>    
-              <hr>
-    `
-  },
-  '5': {
-    title: 'to new paths',
-    date: '2025-02-13',
-    content: `
-    <p> 
-    Today I started a new path, let's see where it'll lead. <br>
-    But after all, you have to have gratitude. <br>
-    So let's listen to some gratitude. <br>
-    <iframe allow='autoplay *; encrypted-media *;' frameborder='0' height='150' style='width:60%;max-width:660px;overflow:hidden;background:transparent;border-radius:12px;margin-bottom:20px;' sandbox='allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation' src="https://embed.music.apple.com/it/album/catalog-of-unabashed-gratitude-feat-bon-iver-ep/1549089052?l=en-GB"></iframe> <br>
-    <iframe style="border-radius:12px;width:60%;height:162px;" src="https://open.spotify.com/embed/album/3XAuX8p3NFyjGBpjP8Adz5?utm_source=generator" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>    
-    </p>
-    This is not just a song. It was first a <a href='https://www.poetryfoundation.org/poems/58762/catalog-of-unabashed-gratitude' target='_blank'> poem</a>. <br>
-    </p>
-    <div class="image-container">
-      <img src="images/posts/catalog_of_unabashed_gratitude.jpg" alt="the new album cover.  all rights reserved. " style="width:100%;max-width:660px;">
-      <div class="caption">
-        the album cover of Catalog of Unabashed Gratitude.  all rights reserved.
-      </div>
-    </div>
-    <hr>
-    `
-  },
-  '6': {
     title: 'a poem by Matsuo Basho',
     date: '2025-02-14',
     content: `
@@ -125,19 +33,6 @@ window.BLOG_ARTICLES = {
           a magazine about books and graphic novels, <br> in a piece named "Paese delle Nevi", <br> 
           by Giovanni Piliarvu. <br>
       </p>
-    `
-  },
-  '7': {
-    title: 'we live in time',
-    date: '2025-02-15',
-    content: `
-        <p> 
-            Today I went to see <a href= 'https://www.imdb.com/title/tt27131358/' target='_blank'> We Live in Time</a>, the new movie by John Crowley starring Florence Pugh and Andrew Garfield. <br>
-            I found it beautiful, along with the soundtrack, composed by <a href='https://brycedessner.com/' target='_blank'> Bryce Dessner</a>. <br>
-          </p>
-            <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style="width:60%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/it/album/we-live-in-time-original-soundtrack/1771503697?l=en-GB"></iframe>  <br>
-            <!-- <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1PrU2ieOlBJw0DOIYeAkIi?utm_source=generator" width="60%" height="450" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>   -->
-          <hr>
     `
   },
 }

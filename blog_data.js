08cccc304718d11f40e3feee5736a674a096673d
Antonio Viscusi <139615741+anvi-git@users.noreply.github.com>
2026-03-12 20:06:38 +0100

    new

--- blog_data.js
+++ blog_data.js
@@ -22,38 +22,8 @@ window.BLOG_ARTICLES = {
 //   },
 '1': {
     title: 'a poem by Matsuo Basho',
-    date: '2025-02-14',
-    content: `
-      <p> 
-          Today I'm sharing a poem by Matsuo Basho. <br>
-          It's a haiku. <br>
-          </p>
-          いざ行かむ <br>
-          雪見 <br>
-          に <br>
-          ころぶ <br>
-          所 <br>
-          まで <br>
-          </p>
-          <p>
-          which sounds like: <br>
-          </p>
-          <p>
-          iza yukan <br>
-          yukimi ni korobu<br>
-          tokoro made <br>
-          </p>
-          <p>
-          and it means, translated in <a href = 'https://www.ucpress.edu/books/basho-2/paper', target='_blank'> English</a> by Andrew Fitzsimons: <br>
-          </p>
-          Let's get up and go <br>
-          view the snow till we come to <br>
-          wherever we fall <br>
-          </p>
-          I stumbled on this haiku while reading <a href='https://www.baldinicastoldi.it/collana/linus/' target='_blank'> Linus</a>, <br> 
-          a magazine about books and graphic novels, <br> in a piece named "Paese delle Nevi", <br> 
-          by Giovanni Piliarvu. <br>
-      </p>
-    `
-  },
+    date: '2026-03-12',
+    excerpt: 'A one-line preview shown on the blog listing.',
+    markdownFile: 'posts/post_1.md'
+}
 }

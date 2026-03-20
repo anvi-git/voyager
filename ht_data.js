b0c37f1712d0efa196a12cceec96a226e8ecf204
anvi-git <antonio.viscusi@edu.unito.it>
2026-03-20 19:11:34 +0100

    new

--- ht_data.js
+++ ht_data.js
@@ -4,168 +4,168 @@ window.HT_ARTICLES = {
     "date": "2026-01-17",
     "excerpt": "of the mathematical curve that changed navigation",
     "markdownFile": "posts/heralding_time/ht_post_how-the-cycloid-saved-the-british.html",
-    "image": "images/cover/ht_cycloid.png"
+    "image": "images/ht/cover/ht_cycloid.png"
   },
   "023": {
     "title": "ATOMOS #3",
     "date": "2026-01-15",
     "excerpt": "a time behind us, a time before us",
     "markdownFile": "posts/heralding_time/ht_post_002-atomos-3.html",
-    "image": "images/cover/atomos3.png"
+    "image": "images/ht/cover/atomos3.png"
   },
   "022": {
     "title": "ATOMOS #2",
     "date": "2026-01-12",
     "excerpt": "a statement by Max Frisch",
     "markdownFile": "posts/heralding_time/ht_post_003-atomos-2.html",
-    "image": "images/cover/atomos2.png"
+    "image": "images/ht/cover/atomos2.png"
   },
   "021": {
     "title": "come la cicloide salvò la marina inglese",
     "date": "2026-01-10",
     "excerpt": "o, della curva matematica che ha cambiato la navigazione",
     "markdownFile": "posts/heralding_time/ht_post_come-la-cicloide-salvo-la-marina.html",
-    "image": "images/cover/ht_cycloid.png"
+    "image": "images/ht/cover/ht_cycloid.png"
   },
   "020": {
     "title": "ATOMOS #1",
     "date": "2026-01-08",
     "excerpt": "an excerpt from Popper's Three views concerning human knowledge",
     "markdownFile": "posts/heralding_time/ht_post_005-atomos-1.html",
-    "image": "images/cover/atomos1.png"
+    "image": "images/ht/cover/atomos1.png"
   },
   "019": {
     "title": "ATOMOS #0",
     "date": "2026-01-05",
     "excerpt": "a thought by Mark Leidner",
     "markdownFile": "posts/heralding_time/ht_post_006-atomos-0.html",
-    "image": "images/cover/atomos0.png"
+    "image": "images/ht/cover/atomos0.png"
   },
   "018": {
     "title": "astronomical history of Christmas",
     "date": "2025-12-21",
     "excerpt": "this is a thorough depiction of the object \"seen\" by the magi, as narrated in the Gospel of Matthew, seen by an astronomical point of view. From Origen to Kepler, from the magi...",
     "markdownFile": "posts/heralding_time/ht_post_007-astronomical-history-of-christmas.html",
-    "image": "images/cover/ht_ahoc.png"
+    "image": "images/ht/cover/ht_ahoc.png"
   },
   "017": {
     "title": "storia astronomica del Natale",
     "date": "2025-12-21",
     "excerpt": "questa è la storia completa della \"cometa di Natale\". dalle idee iniziali di Origene, alle comete di Kepler, ci chiederemo se i magi sono davvero esistiti, cosa hanno in visto i...",
     "markdownFile": "posts/heralding_time/ht_post_storia-astronomica-del-natale.html",
-    "image": "images/cover/ht_ahoc.png"
+    "image": "images/ht/cover/ht_ahoc.png"
   },
   "016": {
     "title": "the responses to Parmenides",
     "date": "2025-12-07",
     "excerpt": "on the motion of the Earth - chapter VIII",
     "markdownFile": "posts/heralding_time/ht_post_009-the-responses-to-parmenides.html",
-    "image": "images/cover/omoe/8.png"
+    "image": "images/ht/cover/omoe/8.png"
   },
   "015": {
     "title": "le risposte a Parmenide",
     "date": "2025-11-30",
     "excerpt": "sul moto della Terra - capitolo VIII",
     "markdownFile": "posts/heralding_time/ht_post_010-le-risposte-a-parmenide.html",
-    "image": "images/cover/smdt/8.png"
+    "image": "images/ht/cover/smdt/8.png"
   },
   "014": {
     "title": "the problem of change",
     "date": "2025-11-23",
     "excerpt": "on the motion of the Earth - chapter VII",
     "markdownFile": "posts/heralding_time/ht_post_011-the-problem-of-change.html",
-    "image": "images/cover/omoe/7.png"
+    "image": "images/ht/cover/omoe/7.png"
   },
   "013": {
     "title": "il problema del cambiamento",
     "date": "2025-11-16",
     "excerpt": "sul moto della Terra - capitolo VII",
     "markdownFile": "posts/heralding_time/ht_post_012-il-problema-del-cambiamento.html",
-    "image": "images/cover/smdt/7.png"
+    "image": "images/ht/cover/smdt/7.png"
   },
   "012": {
     "title": "Anaximander’s indefinite principle and Anaximenes’ cosmic breath",
     "date": "2025-11-05",
     "excerpt": "on the motion of the Earth - chapter VI",
     "markdownFile": "posts/heralding_time/ht_post_anaximanders-indefinite-principle.html",
-    "image": "images/cover/omoe/6.png"
+    "image": "images/ht/cover/omoe/6.png"
   },
   "011": {
     "title": "il principio indefinito di Anassimandro e il respiro cosmico di Anassimene",
     "date": "2025-11-02",
     "excerpt": "sul moto della Terra - capitolo VI",
     "markdownFile": "posts/heralding_time/ht_post_il-principio-indefinito-di-anassimandro.html",
-    "image": "images/cover/smdt/6.png"
+    "image": "images/ht/cover/smdt/6.png"
   },
   "010": {
     "title": "Thales and the water",
     "date": "2025-10-22",
     "excerpt": "on the motion of the Earth - chapter V",
     "markdownFile": "posts/heralding_time/ht_post_on-the-motion-of-the-earth-thales.html",
-    "image": "images/cover/omoe/5.png"
+    "image": "images/ht/cover/omoe/5.png"
   },
   "009": {
     "title": "Talete e l'acqua",
     "date": "2025-10-19",
     "excerpt": "sul moto della Terra - capitolo V",
     "markdownFile": "posts/heralding_time/ht_post_sul-moto-della-terra-talete-e-lacqua.html",
-    "image": "images/cover/smdt/5.png"
+    "image": "images/ht/cover/smdt/5.png"
   },
   "008": {
     "title": "Homer, Hesiod, and a proto-philosopher",
     "date": "2025-10-15",
     "excerpt": "on the motion of the Earth - chapter IV",
     "markdownFile": "posts/heralding_time/ht_post_on-the-motion-of-the-earth-homer.html",
-    "image": "images/cover/omoe/4.png"
+    "image": "images/ht/cover/omoe/4.png"
   },
   "007": {
     "title": "Omero, Esiodo, e un proto-filosofo",
     "date": "2025-10-12",
     "excerpt": "sul moto della Terra - capitolo IV",
     "markdownFile": "posts/heralding_time/ht_post_sul-moto-della-terra-omero-esiodo.html",
-    "image": "images/cover/smdt/4.png"
+    "image": "images/ht/cover/smdt/4.png"
   },
   "006": {
     "title": "constants, collections, synthesis",
     "date": "2025-10-08",
     "excerpt": "on the motion of the Earth - chapter III",
     "markdownFile": "posts/heralding_time/ht_post_on-the-motion-of-the-earth-constants.html",
-    "image": "images/cover/omoe/3.png"
+    "image": "images/ht/cover/omoe/3.png"
   },
   "005": {
     "title": "costanti, raccolte, sintesi",
     "date": "2025-10-05",
     "excerpt": "sul moto della Terra - capitolo III",
     "markdownFile": "posts/heralding_time/ht_post_sul-moto-della-terra-costanti-raccolte.html",
-    "image": "images/cover/smdt/3.png"
+    "image": "images/ht/cover/smdt/3.png"
   },
   "004": {
     "title": "three rivers, nine spheres, one Creator",
     "date": "2025-10-01",
     "excerpt": "on the motion of the Earth - chapter II",
     "markdownFile": "posts/heralding_time/ht_post_eng-on-the-motion-of-the-earth-three.html",
-    "image": "images/cover/omoe/2.png"
+    "image": "images/ht/cover/omoe/2.png"
   },
   "003": {
     "title": "tre fiumi, nove sfere, un Creatore",
     "date": "2025-09-28",
     "excerpt": "sul moto della Terra - capitolo II",
     "markdownFile": "posts/heralding_time/ht_post_ita-sul-moto-della-terra-tre-fiumi.html",
-    "image": "images/cover/smdt/2.png"
+    "image": "images/ht/cover/smdt/2.png"
   },
   "002": {
     "title": "Sun, night, and gods",
     "date": "2025-09-24",
     "excerpt": "on the motion of the Earth - chapter I",
     "markdownFile": "posts/heralding_time/ht_post_eng-on-the-motion-of-the-earth-sun.html",
-    "image": "images/cover/omoe/1.png"
+    "image": "images/ht/cover/omoe/1.png"
   },
   "001": {
     "title": "il Sole, la notte, gli dei",
     "date": "2025-09-21",
     "excerpt": "sul moto della Terra - capitolo I",
     "markdownFile": "posts/heralding_time/ht_post_ita-sul-moto-della-terra-il-sole.html",
-    "image": "images/cover/smdt/1.png"
+    "image": "images/ht/cover/smdt/1.png"
   },
   "000": {
     "title": "Welcome to Heralding Time",

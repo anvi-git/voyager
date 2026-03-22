8cd30e69b0cd4479a44a5545120ae55fff36c78f
anvi-git <antonio.viscusi@edu.unito.it>
2026-03-22 14:35:11 +0100

    clean

--- updateDate.js
+++
@@ -1,8 +0,0 @@
-function updateDate() {
-  const dateElement = document.getElementById("current-date");
-  if (dateElement) {
-      const options = { year: 'numeric', month: 'short', day: '2-digit' };
-      const today = new Date().toLocaleDateString('en-US', options);
-      dateElement.textContent = today;
-  }
-}
